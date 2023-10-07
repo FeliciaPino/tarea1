ddc9c6b268670b719a7bc65814be695ea21a985c
Felicia <faguin2022@udec.cl>
2023-10-06 21:15:32 -0300

    se implementa la clase Cliente, se le añaden adociacions a la clase Direccion

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,24 @@
 public class Cliente {
+    private String nombre;
+    private String rut;
+    private Direccion direccion;
+    public Cliente(String nombre, String rut){
+        this.nombre = nombre;
+        this.rut = rut;
+    }
+    public String getNombre(){return nombre;}
+    public String getRut(){return rut;}
+
+    public Direccion getDireccion() {return direccion;}
+    public void setNombre(String nombre){this.nombre = nombre;}
+    public void setRut(String rut){this.rut = rut;}
+    public void setDireccion(Direccion direccion){this.direccion = direccion;}
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nombre='" + nombre + '\'' +
+                ", rut='" + rut + '\'' +
+                '}';
+    }
 }
--- src/Direccion.java
+++ src/Direccion.java
@@ -1,7 +1,33 @@
+import java.util.ArrayList;
 public class Direccion {
     private String direccion;
+    private ArrayList<Cliente> clientes;
+    private ArrayList<DocTributario> documentos;
     public Direccion(String direccion){
         this.direccion = direccion;
+    }
+
+    public void addCliente(Cliente cliente){
+        clientes.add(cliente);
+    }
+    public void addDocumento(DocTributario documento){
+        documentos.add(documento);
+    }
+    public int removeCliente(Cliente cliente){
+        try{
+            clientes.remove(cliente);
+            return 0;
+        }catch (Exception e){
+            return -1;
+        }
+    }
+    public int removeDocumento(DocTributario documento){
+        try{
+            documentos.remove(documento);
+            return 0;
+        }catch (Exception e){
+            return -1;
+        }
     }
 
     public String getDireccion() {return direccion;}
